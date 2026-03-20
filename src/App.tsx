5e0a6c571f8f1f940e89094ac6e4622fae88128e
nandhakumar <nanthablackbird@gmail.com>
2026-03-20 15:15:24 +0530

    AI Update: Main application layout stitching all components t...

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Pricing from './components/Pricing';
+import Testimonials from './components/Testimonials';
+import CTA from './components/CTA';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <Hero />
+        <Features />
+        <Testimonials />
+        <Pricing />
+        <CTA />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
